6dd660293ab591250079ed65039882eed49325d6
blaspheme <342843739@qq.com>
2021-09-19 04:30:10 +0800

    增加run脚本的自动删除容器功能

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+docker stop simplest_log_server || true 
+docker rm simplest_log_server || true 
 docker run -d -p 10001:8000  -v /var/run/docker.sock:/var/run/docker.sock --name simplest_log_server erudite/simplest_server_nginx:1.0.0
-docker run -it --rm  -v /var/run/docker.sock:/var/run/docker.sock erudite/simplest_server_nginx:1.0.0 sh
