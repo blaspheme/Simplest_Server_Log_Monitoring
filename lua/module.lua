d131004770ab87af49b2f85e73649aca00106b5d
blaspheme <342843739@qq.com>
2021-09-19 07:24:05 +0800

    采用 docker ps --format

--- lua/module.lua
+++ lua/module.lua
@@ -2,22 +2,20 @@ module = {}
 
 -- 函数定义
 function module.index() -- 首页函数
-    local docker_ps = io.popen([[/usr/local/bin/docker ps -a | awk -F" " '{print $1,$2}']])
+    local docker_ps = io.popen(
+        [[/usr/local/bin/docker ps -a --format "{{.ID}}\t{{.Image}}\t{{.CreatedAt}}\t{{.Status}}\t{{.Ports}}"]])
     ngx.say([[<script type="text/javascript">function clickDockerLogs(n,container_id) {
         window.location.href=`?methods=docker_logs&n=${n}&container_id=${container_id}`;
     }</script>]])
     local line = docker_ps:read()
-    local first_line_flag = true
-    while line do
+    ngx.say([[<span>ID  Image   CreatedAt   Status  Ports </span>]]) -- 表头
+    ngx.say("<br>")
+    while line do -- 表内容
         ngx.say(line)
-        if first_line_flag then -- 第一行不增加按钮 
-            first_line_flag = false
-        else -- 非第一行增加按钮
-            for word in string.gmatch(line, "%w+") do
-                -- https://www.codenong.com/9643311/
-                ngx.say(string.format([[<input type="button" value="click" onclick="clickDockerLogs('100','%s')" />]], word))
-                break
-            end
+        for word in string.gmatch(line, "[^%s]+") do
+            -- https://www.codenong.com/9643311/
+            ngx.say(string.format([[<input type="button" value="click" onclick="clickDockerLogs('100','%s')" />]], word))
+            break
         end
         ngx.say("<br>")
         line = docker_ps:read() -- 读取下一行
@@ -35,3 +33,8 @@ function module.docker_logs(n, container_id)
 end
 
 return module
+
+-- for word in string.gmatch([[246a23818b76    yc_springboot:test  2021-09-18 18:51:54 +0800 CST   Up 12 hours 0.0.0.0:8311->8211/tcp, :::8311->8211/tcp]],"[^%s]+") do
+--     print(word)
+--     print("xxxxxxxxxxxxxxxx")
+-- end
